abe09c437c8a76bbbb65494799fb487fe85524a4
Emma Goodman <emmagoodmannyc@gmail.com>
2019-01-30 02:06:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const takeANumber = (line, name) => {
   line.push(name);
-  return `Welcome, ${name}. You are number ` + line.length ` in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 };
