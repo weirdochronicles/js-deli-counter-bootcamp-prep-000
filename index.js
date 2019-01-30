8d5f813ee513ce1ec98bfac4035d19f4f2fd8c69
Emma Goodman <emmagoodmannyc@gmail.com>
2019-01-30 01:27:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const takeANumber = (line, name) => {
   let customers = [];
-  for (let i = 0; i < line.length;  i++) {
+  for (let i = 0; i <= line.length;  i++) {
   customers.push(`Welcome, ${name[i]} You are number ${i} in line.`)
   }
   return customers
