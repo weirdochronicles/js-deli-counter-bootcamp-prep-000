c9abde74f791c1be24bf42d897c07eb518824fce
Emma Goodman <emmagoodmannyc@gmail.com>
2019-01-30 02:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const takeANumber = (line, name) => {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`
 };
+
+const nowServing = katzDeliLine => {
+  if (katzDeliLine.length === 0) {
+    return ``
+  }
+}
